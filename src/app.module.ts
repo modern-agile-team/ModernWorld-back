965963a459b43ab9a00a5833dff625e683c6e7a1
JYE <jo1814@naver.com>
2024-03-17 23:14:08 +0900

    setting/#3: set .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  // isGlobal: ConfigModule을 AppModul 이외의 모듈에서 반복 import할 필요 없는 전역 모듈로 설정.
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
+  // 앵간하면 nestJS에서 권장하는 방법인 ConfigService를 이용하여 환경변수 값을 사용해야함.
 })
 export class AppModule {}
