eaa297275bb1986e3ccfae83a5a2429ad193a69b
JYE <jo1814@naver.com>
2024-04-05 21:14:02 +0900

    feature/#5 : get oneUser with name, point, achievementTitle.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
+import { UsersService } from "./users.service";
 
 @Controller("users")
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get(":no")
+  async getOneUserWithNameAndPointAndAchievementTitle(
+    @Param("no", ParseIntPipe) no: number,
+  ) {
+    return this.userService.getUserNameAndCurrentPointAndAccumulatinPoint(no);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async getUserNameAndCurrentPointAndAccumulatinPoint(no: number) {
+    const prisma = new PrismaClient();
+    const result = await prisma.user.findUnique({
+      where: {
+        no,
+      },
+
+      select: {
+        nickname: true,
+        currentPoint: true,
+        accumulationPoint: true,
+        user_achievement: {
+          select: {
+            achievement: { select: { title: true } },
+          },
+        },
+      },
+    });
+
+    return result;
+  }
+}
