2982d12937c806395cbeb8cc3bb1edcc59bcfc86
JYE <jo1814@naver.com>
2024-04-11 11:00:06 +0900

    refactor/#5: Modifying relative addresses

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
-import { PrismaService } from "src/prisma.service";
-
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService],
 })
 export class UsersModule {}
