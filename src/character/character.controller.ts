af64701133cd7748a079432e22d2601ddbcf529e
JYE <jo1814@naver.com>
2024-05-01 16:31:30 +0900

    feature/#24: constructor characterService

--- src/character/character.controller.ts
+++ src/character/character.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from "@nestjs/common";
+import { CharacterService } from "./character.service";
 
 @Controller("character")
-export class CharacterController {}
+export class CharacterController {
+  constructor(private readonly characterServcie: CharacterService) {}
+}
