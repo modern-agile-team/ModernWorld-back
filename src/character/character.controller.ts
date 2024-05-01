454db350322cb2d890bc8277bcd035fcea4dba22
JYE <jo1814@naver.com>
2024-05-01 16:37:14 +0900

    feature/#24: add character Controller

--- src/character/character.controller.ts
+++ src/character/character.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from "@nestjs/common";
 import { CharacterService } from "./character.service";
 
 @Controller("character")
 export class CharacterController {
   constructor(private readonly characterServcie: CharacterService) {}
+
+  @Get("")
+  getAllCharactersBySpeices(@Query("species") species: string) {
+    //요놈 type, User에서 만든 animal enum 나중에 써라
+  }
+
+  @Post(":characterNo")
+  createCharacter(@Param("characterNo", ParseIntPipe) characterNo: number) {}
+
+  @Patch("characterNo")
+  updateCharacterStatus(
+    @Param("characterNo", ParseIntPipe) characterNo: number,
+  ) {}
 }
