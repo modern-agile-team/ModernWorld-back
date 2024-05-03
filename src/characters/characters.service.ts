9698caa210b1b3803da105341db59bbdc259b369
JYE <jo1814@naver.com>
2024-05-03 18:39:39 +0900

    feature/#24 add addCharacter logic

--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -27,11 +27,13 @@ export class CharactersService {
      * 유저가 캐릭터를 사는 로직
      * 1. 해당번호의 캐릭터가 캐릭터 테이블에 실제로 존재하는지 확인
      *
-     * 2. 유저가 캐릭터를 살만큼 충분한 포인트를 가지고 있는지 확인
+     * 2. 이미 가지고 있는지 확인
      *
-     * 3. 캐릭터 보관함의 캐릭터추가
+     * 3. 유저가 캐릭터를 살만큼 충분한 포인트를 가지고 있는지 확인
      *
-     * 4. 유저 포인트 감소
+     * 4. 캐릭터 보관함에 캐릭터추가
+     *
+     * 5. 유저 포인트 감소
      */
 
     const character =
@@ -39,6 +41,16 @@ export class CharactersService {
 
     if (!character) {
       throw new NotFoundException("There is no such character.");
+    }
+
+    const isThereCharacter =
+      await this.characterLocker.findOneCharacterFromInventory(
+        userNo,
+        characterNo,
+      );
+
+    if (isThereCharacter) {
+      throw new ForbiddenException("User already has this character");
     }
 
     const { currentPoint } = await this.userRepository.findUserPoint(userNo);
