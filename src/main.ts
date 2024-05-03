47707ec04f87301c2f9bfff951350fb5eaabfe7f
JYE <jo1814@naver.com>
2024-05-03 14:06:43 +0900

    feature/#26: add swagger into bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Modern World API")
+    .setDescription("API of Modern World")
+    .setVersion("0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
