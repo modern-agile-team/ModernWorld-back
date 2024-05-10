e6a055ea0dad48a2cda0149150652266e95b772d
JYE <jo1814@naver.com>
2024-05-10 19:12:28 +0900

    delete/#30: itemModule import 구문 삭제

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -2,11 +2,8 @@ import { Module } from "@nestjs/common";
 import { ItemsService } from "./items.service";
 import { ItemsController } from "./items.controller";
 import { ItemsRepository } from "./items.repository";
-import { InventoryModule } from "src/inventory/inventory.module";
-import { UsersModule } from "src/users/users.module";
 
 @Module({
-  imports: [InventoryModule, UsersModule],
   controllers: [ItemsController],
   providers: [ItemsService, ItemsRepository],
   exports: [ItemsRepository],
