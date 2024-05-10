9c6a947efbf8d06ceb83593d55686f91d4f147fd
JYE <jo1814@naver.com>
2024-05-10 20:12:50 +0900

    delete/#30: usersModule 임포트 제거

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,8 @@ import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 import { UsersRepository as UsersRepository } from "./users.repository";
-import { CharacterLockerModule } from "src/characterLocker/characterLocker.module";
 
 @Module({
-  imports: [CharacterLockerModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersRepository],
