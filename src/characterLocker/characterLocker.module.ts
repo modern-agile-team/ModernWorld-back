5bcad880f06e1f83ea191599268011d3a8f5d0a2
JYE <jo1814@naver.com>
2024-05-10 20:08:18 +0900

    feature/#30: characterLockerModule 임포트 추가

--- src/characterLocker/characterLocker.module.ts
+++ src/characterLocker/characterLocker.module.ts
@@ -2,8 +2,11 @@ import { Module } from "@nestjs/common";
 import { CharacterLockerRepository } from "./characterLocker.repository";
 import { CharacterLockerController } from "./characterLocker.controller";
 import { CharacterLockerService } from "./characterLocker.service";
+import { CharactersModule } from "src/characters/characters.module";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
+  imports: [CharactersModule, UsersModule],
   controllers: [CharacterLockerController],
   providers: [CharacterLockerRepository, CharacterLockerService],
   exports: [CharacterLockerRepository],
