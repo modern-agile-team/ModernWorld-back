c6f54c8dd243e05cfb36d271a9f50532f4bfcad5
JYE <jo1814@naver.com>
2024-05-10 20:12:10 +0900

    modify/#30: charactersModule import제거 및 export 추가

--- src/characters/characters.module.ts
+++ src/characters/characters.module.ts
@@ -2,12 +2,10 @@ import { Module } from "@nestjs/common";
 import { CharactersController } from "./characters.controller";
 import { CharactersService } from "./characters.service";
 import { CharactersRepository } from "./characters.repository";
-import { UsersModule } from "src/users/users.module";
-import { CharacterLockerModule } from "src/characterLocker/characterLocker.module";
 
 @Module({
-  imports: [UsersModule, CharacterLockerModule],
   controllers: [CharactersController],
   providers: [CharactersService, CharactersRepository],
+  exports: [CharactersRepository],
 })
 export class CharactersModule {}
