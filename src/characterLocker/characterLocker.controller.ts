903737588159dfe93f3b94b2debd9dd4a93105e0
JYE <jo1814@naver.com>
2024-05-10 20:23:48 +0900

    feature/#30: characterLocker ApiTags 추가

--- src/characterLocker/characterLocker.controller.ts
+++ src/characterLocker/characterLocker.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 import { CharacterLockerService } from "./characterLocker.service";
+import { ApiTags } from "@nestjs/swagger";
 
 @Controller("characterLocker")
+@ApiTags("CharacterLocker")
 export class CharacterLockerController {
   constructor(
     private readonly characterLockerService: CharacterLockerService,
