90ef9d70333e0829f8547284f739a4bb52ace119
JYE <jo1814@naver.com>
2024-05-10 20:28:34 +0900

    feature/#30: characterLocker 명세서 적용

--- src/characterLocker/characterLocker.controller.ts
+++ src/characterLocker/characterLocker.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 import { CharacterLockerService } from "./characterLocker.service";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller("characterLocker")
 @ApiTags("CharacterLocker")
@@ -9,6 +9,10 @@ export class CharacterLockerController {
     private readonly characterLockerService: CharacterLockerService,
   ) {}
   @Post(":characterNo")
+  @ApiOperation({
+    summary: "캐릭터 구매 API",
+    description: "캐릭터를 구매하여 characterLocker테이블에 등록합니다.",
+  })
   createCharacter(@Param("characterNo", ParseIntPipe) characterNo: number) {
     const userNo = 1;
 
@@ -16,6 +20,11 @@ export class CharacterLockerController {
   }
 
   @Patch(":characterNo")
+  @ApiOperation({
+    summary: "캐릭터 사용 API",
+    description:
+      "캐릭터를 사용합니다. 이전에 사용했던 캐릭터는 사용해제됩니다.",
+  })
   updateCharacterStatus(
     @Param("characterNo", ParseIntPipe) characterNo: number,
   ) {
