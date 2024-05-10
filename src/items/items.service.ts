02ed633249d07d3136d1c66454442940c4f98969
JYE <jo1814@naver.com>
2024-05-10 19:13:17 +0900

    delete/#30: itemService에 있던 구문 inventroy에 옮김에 따라 삭제

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,19 +1,8 @@
-import {
-  ConflictException,
-  ForbiddenException,
-  Injectable,
-  NotFoundException,
-} from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { ItemsRepository } from "./items.repository";
-import { InventoryRepository } from "src/inventory/inventory.repository";
-import { UsersRepository } from "src/users/users.repository";
 @Injectable()
 export class ItemsService {
-  constructor(
-    private readonly itemsRepository: ItemsRepository,
-    private readonly inventoryRepository: InventoryRepository,
-    private readonly usersRepository: UsersRepository,
-  ) {}
+  constructor(private readonly itemsRepository: ItemsRepository) {}
 
   async getOneItem(itemNo: number): Promise<object> {
     const result = await this.itemsRepository.getOneItem(itemNo);
@@ -22,71 +11,5 @@ export class ItemsService {
 
   async getUserItems(theme?: string): Promise<object> {
     return await this.itemsRepository.getAllItems(theme);
-  }
-
-  async buyOneItem(userNo: number, itemNo: number): Promise<boolean> {
-    /**
-     * 아이템을 사는 로직
-     *
-     * 1. 그 아이템이 실제로 아이템 테이블에 존재하는 지 확인 (itemsRepository)ㅇ
-     *
-     * 2. 아이템을 이미 보유하고있는지 확인하는 로직 추가 있다면 구매불가 (inventoryRepository)ㅇ
-     *
-     * 3. 포인트가 아이템 가격보다 높은지 확인 (usersRepository)
-     *
-     * 4. 물건 추가 (inventoryRepository)
-     *
-     * 5. 포인트 감소 (usersRepository)
-     *
-     * 4번 5번의 과정은 트랜잭션을 한번에 묶자.
-     */
-
-    const item = await this.itemsRepository.getOneItem(itemNo);
-
-    if (!item) {
-      throw new NotFoundException("Item doesn't exist.");
-    }
-
-    const checkInventoryItem = await this.inventoryRepository.findOneItem(
-      userNo,
-      itemNo,
-    );
-
-    if (checkInventoryItem) {
-      throw new ConflictException("User already owns the item.");
-    }
-
-    const { currentPoint } = await this.usersRepository.findUserPoint(userNo);
-
-    if (currentPoint < item.price) {
-      throw new ForbiddenException("User doesn't have enough point.");
-    }
-
-    await this.inventoryRepository.addOneItem(userNo, itemNo);
-
-    await this.usersRepository.updateUserCurrentPoint(userNo, -item.price);
-
-    return true;
-  }
-
-  async useItemDisuseOthers(userNo: number, itemNo: number): Promise<boolean> {
-    /**
-     * user가 이미 같은 타입의 아이템을 사용하고 있다면 자동으로 바꿔줘야함
-     * 즉, 사용할 아이템의 status를 true로 변경하고 기존의 아이템의 status를 false로 전환
-     * 이것 역시 트랜잭션을 이용할것
-     */
-    const item = await this.inventoryRepository.findOneItem(userNo, itemNo);
-
-    if (!item) {
-      throw new NotFoundException("User doesn't have the item");
-    }
-
-    const { type } = await this.itemsRepository.getItemType(itemNo);
-
-    await this.inventoryRepository.disuseOtherItems(userNo, type);
-
-    await this.inventoryRepository.updateItemStatus(userNo, itemNo);
-
-    return true;
   }
 }
