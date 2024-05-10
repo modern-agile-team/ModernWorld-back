3403cb12442d69f392ce5ab8f8b349a021c1f766
JYE <jo1814@naver.com>
2024-05-10 18:46:44 +0900

    feature/#30: userContorller ApiTags, Operation추가

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,16 +8,24 @@ import {
   Query,
 } from "@nestjs/common";
 import { ItemsService } from "./items.service";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller("items")
+@ApiTags("Items")
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
   @Get()
+  @ApiOperation({
+    summary: "아이템 테마별로 가져오기 API",
+  })
   getUserItems(@Query("theme") theme?: string) {
     return this.itemsService.getUserItems(theme);
   }
 
   @Get(":itemNo")
+  @ApiOperation({
+    summary: "특정 아이템 가져오기 API",
+  })
   getOneItem(@Param("itemNo", ParseIntPipe) itemNo: number) {
     return this.itemsService.getOneItem(itemNo);
   }
