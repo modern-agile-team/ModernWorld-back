81c05d7502d06a023b274b58ed422ad26cc8cca5
JYE <jo1814@naver.com>
2024-05-10 15:43:20 +0900

    refactor/#30: presents Module에 user, item 임포트

--- src/presents/presents.module.ts
+++ src/presents/presents.module.ts
@@ -3,11 +3,12 @@ import { PresentsController } from "./presents.controller";
 import { PresentsService } from "./presents.service";
 import { PresentsRepository } from "./presents.repository";
 import { InventoryModule } from "src/inventory/inventory.module";
+import { UsersModule } from "src/users/users.module";
+import { ItemsModule } from "src/items/items.module";
 
 @Module({
-  imports: [InventoryModule],
+  imports: [InventoryModule, UsersModule, ItemsModule],
   controllers: [PresentsController],
   providers: [PresentsService, PresentsRepository],
-  exports: [PresentsService],
 })
 export class PresentsModule {}
