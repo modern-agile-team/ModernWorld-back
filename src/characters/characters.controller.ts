6bedf094366c35717e34f06c7274fa9bf247bdaf
JYE <jo1814@naver.com>
2024-05-10 20:38:57 +0900

    feature/#30: charactes 명세서 추가 및 EnumPipe에 optional 옵션값 추가

--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -8,7 +8,7 @@ import {
 } from "@nestjs/common";
 import { CharactersService } from "./characters.service";
 import { Animal } from "src/common/enum/animal-enum";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller("characters")
 @ApiTags("Characters")
@@ -16,8 +16,13 @@ export class CharactersController {
   constructor(private readonly charactersServcie: CharactersService) {}
 
   @Get()
+  @ApiOperation({
+    summary: "모든 캐릭터 불러오기 API",
+    description: "캐릭터를 불러옵니다. species는 cat, dog",
+  })
   getCharactersBySpeices(
-    @Query("species", new ParseEnumPipe(Animal)) species?: Animal,
+    @Query("species", new ParseEnumPipe(Animal, { optional: true }))
+    species?: Animal,
   ) {
     //요놈 type, User에서 만든 animal enum 나중에 써라
 
@@ -25,6 +30,11 @@ export class CharactersController {
   }
 
   @Get(":characterNo")
+  @ApiOperation({
+    summary: "캐릭터 사용 API",
+    description:
+      "캐릭터를 사용합니다. 이전에 사용했던 캐릭터는 사용해제됩니다.",
+  })
   getOneCharacter(@Param("characterNo", ParseIntPipe) characterNo: number) {
     return this.charactersServcie.getOneCharacter(characterNo);
   }
