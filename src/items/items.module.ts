0f2a0846d29d4dc7c78aa32b67d6023b4cfa5271
JYE <jo1814@naver.com>
2024-05-10 15:44:03 +0900

    feature/#30: item Module Repo export함

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -9,5 +9,6 @@ import { UsersModule } from "src/users/users.module";
   imports: [InventoryModule, UsersModule],
   controllers: [ItemsController],
   providers: [ItemsService, ItemsRepository],
+  exports: [ItemsRepository],
 })
 export class ItemsModule {}
