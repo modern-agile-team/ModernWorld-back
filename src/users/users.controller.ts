f538d5aa1186fb3db579aa70928f5cdac060ed7a
JYE <jo1814@naver.com>
2024-05-14 20:55:49 +0900

    delete/#30: userController 쓸모없는 주석 제거

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,19 +16,9 @@ import { ApiOperation, ApiParam, ApiTags } from "@nestjs/swagger";
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  //offset 기반 pagination
-  //인기(좋아요), 최신유저, 랭킹(누적포인트 랭킹)
   @Get()
   @ApiOperation({ summary: "유저 불러오기 API" })
   getUsersByAnimal(@Query() queryParams: GetUsersByAnimalDto) {
-    /**
-     * pageNo는 페이지의 번호
-     * take는 몇개씩 가져올것인가
-     * animal은 그 사람이 현재 사용중인 동물, 어차피 개 고양이밖에 없음 만약 주지않는다면 모든 유저를 불러옴
-     * orderByField는 어떤 식으로 정렬할것인지에 대한 정보
-     */
-
-    //VALIDATION !!!!!! orderByField의 값은 정해져있어야함, like, createdAt, accumulationPoint 이 세개로 한정해야함
     return this.usersService.getUsersByAnimal(queryParams);
   }
 
@@ -46,14 +36,12 @@ export class UsersController {
     return this.usersService.getUserNamePointTitleCharacter(userNo);
   }
 
-  //유저 출석부 조회
   @Get(":userNo/attendance")
   @ApiOperation({ summary: "유저 출석부 조회 API" })
   getOneUserAttendance(@Param("userNo", ParseIntPipe) userNo: number) {
     return this.usersService.getUserAttendance(userNo);
   }
 
-  //유저 출석부 체크
   @Patch(":userNo/attendance")
   @ApiOperation({ summary: "유저 출석부 체크 API" })
   markUserAttendance(@Param("userNo", ParseIntPipe) userNo: number) {
@@ -62,7 +50,6 @@ export class UsersController {
     return this.usersService.markUserAttendance(tokenUserNo, userNo);
   }
 
-  //유저 닉네임, 자기소개 Update
   @Patch(":userNo")
   updateUser(
     @Param("userNo", ParseIntPipe) userNo: number,
