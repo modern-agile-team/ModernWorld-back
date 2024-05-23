422ec7754bb3bc9608c210d9a617baf9f9ba3272
JYE <jo1814@naver.com>
2024-05-23 19:03:55 +0900

    userDto 글자수 제한 명세서 표기

--- src/users/dtos/update-user-nickname.dto.ts
+++ src/users/dtos/update-user-nickname.dto.ts
@@ -1,11 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsString, MaxLength, MinLength } from "class-validator";
+import { MaxLength, MinLength } from "class-validator";
 
 export class UpdateUserNicknameDto {
   @ApiProperty({
     name: "nickname",
     description: "닉네임",
     example: "엄준식",
+    minimum: 1,
+    maximum: 10,
   })
   @MinLength(1)
   @MaxLength(10)
