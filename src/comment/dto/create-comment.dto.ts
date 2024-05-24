c2092f57a23a839fbeb5734767dcc7b34eb5bf85
cyoure <wwj9559@gmail.com>
2024-05-24 20:08:30 +0900

    refactor/#22: default를 example로 수정

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -4,6 +4,6 @@ import { IsNotEmpty, MaxLength } from "class-validator";
 export class CreateCommentDto {
   @IsNotEmpty()
   @MaxLength(100)
-  @ApiProperty({ description: "방명록 내용", default: "밥먹고 싶어요" })
+  @ApiProperty({ description: "방명록 내용", example: "밥먹고 싶어요" })
   content: string;
 }
