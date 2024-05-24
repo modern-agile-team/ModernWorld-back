937435369606ca59fc4baeeca4275dedcbb75829
cyoure <wwj9559@gmail.com>
2024-05-24 20:19:24 +0900

    refactor/#22: swagger 방명록 불러오기 안 되는 부분 수정

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,8 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, MaxLength } from "class-validator";
+import { MaxLength, MinLength } from "class-validator";
 
 export class CreateCommentDto {
-  @IsNotEmpty()
+  @MinLength(1)
   @MaxLength(100)
   @ApiProperty({ description: "방명록 내용", example: "밥먹고 싶어요" })
   content: string;
