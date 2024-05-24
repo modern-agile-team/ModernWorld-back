5e9f77d8c9345fd668caebb28cc0533fa95fb625
cyoure <wwj9559@gmail.com>
2024-05-24 20:01:12 +0900

    refactor/#22: swagger 설정

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -13,12 +13,18 @@ import { CommentService } from "./comment.service";
 import { CreateCommentDto } from "./dto/create-comment.dto";
 import { UpdateCommentDto } from "./dto/update-comment.dto";
 import { GetCommentDto } from "./dto/get-comment.dto";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller("comment")
+@ApiTags("Comment")
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
   @Post(":receiverNo")
+  @ApiOperation({
+    summary: "방명록 생성하는 API",
+    description: "방명록을 생성합니다.",
+  })
   createComment(
     @Param("receiverNo", ParseIntPipe) receiverNo: number,
     @Body() content: CreateCommentDto,
@@ -28,12 +34,20 @@ export class CommentController {
   }
 
   @Get()
+  @ApiOperation({
+    summary: "방명록 조회하는 API",
+    description: "방명록을 조회합니다.",
+  })
   getComment(@Query() queryParams: GetCommentDto) {
     const userNo = 1;
     return this.commentService.getComment(userNo, queryParams);
   }
 
   @Patch(":commnetNo")
+  @ApiOperation({
+    summary: "방명록 수정하는 API",
+    description: "방명록을 수정합니다.",
+  })
   updateComment(
     @Param("commnetNo", ParseIntPipe) commentNo: number,
     @Body() content: UpdateCommentDto,
@@ -42,6 +56,10 @@ export class CommentController {
   }
 
   @Delete(":commnetNo")
+  @ApiOperation({
+    summary: "방명록 삭제하는 API",
+    description: "방명록을 삭제합니다. 하지만 db에는 존재합니다.",
+  })
   softDeleteComment(@Param("commnetNo", ParseIntPipe) commentNo: number) {
     return this.commentService.softDeleteComment(commentNo);
   }
--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, MaxLength } from "class-validator";
 
 export class CreateCommentDto {
   @IsNotEmpty()
   @MaxLength(100)
+  @ApiProperty({ description: "방명록 내용", default: "밥먹고 싶어요" })
   content: string;
 }
