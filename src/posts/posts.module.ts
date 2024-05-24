834f3d42634a4e1816b9537b7615045b02df0294
JYE <jo1814@naver.com>
2024-05-24 15:15:55 +0900

    feature/#31: import usersModule

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { PostsService } from "./posts.service";
 import { PostsController } from "./posts.controller";
 import { PostsRepository } from "./posts.repositroy";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
+  imports: [UsersModule],
   controllers: [PostsController],
   providers: [PostsService, PostsRepository],
 })
