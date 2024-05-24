213a999891d12addf066014411550075fc14b1fa
JYE <jo1814@naver.com>
2024-05-24 15:34:34 +0900

    feature/#30: create-post DTO ApiPropery 설정

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { MaxLength, MinLength } from "class-validator";
 
 export class createOnePostDto {
+  @ApiProperty({
+    name: "content",
+    example: "김뿡우",
+  })
   @MaxLength(100)
   @MinLength(1)
   content: string;
