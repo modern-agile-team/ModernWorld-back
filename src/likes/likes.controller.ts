041913cf293877165a75b9934b8ca5df59794e0d
JYE <jo1814@naver.com>
2024-05-28 11:05:49 +0900

    feature/#42: con, likesService 주입

--- src/likes/likes.controller.ts
+++ src/likes/likes.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Delete, Post } from "@nestjs/common";
+import { LikesService } from "./likes.service";
 
 @Controller("like")
 export class LikesController {
+  constructor(private readonly likesService: LikesService) {}
   @Post("")
   test() {}
 
