3aae3914d822c9a9b01c03e0a34b1cf1393505ae
JYE <jo1814@naver.com>
2024-05-29 12:35:03 +0900

    feature/#42: repo 좋아요 생성, 삭제 로직 추가

--- src/likes/likes.repository.ts
+++ src/likes/likes.repository.ts
@@ -1,11 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaPromise, like } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class LikesRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  createOneLike() {}
+  createOneLike(senderNo: number, receiverNo: number): PrismaPromise<like> {
+    return this.prisma.like.create({ data: { senderNo, receiverNo } });
+  }
 
-  deleteOneLike() {}
+  deleteOneLike(
+    senderNo: number,
+    receiverNo: number,
+  ): PrismaPromise<{ count: number }> {
+    return this.prisma.like.deleteMany({ where: { senderNo, receiverNo } });
+  }
 }
