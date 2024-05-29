ce9a1d30bcd7643d5c51ff68571fcfe70018403c
JYE <jo1814@naver.com>
2024-05-29 20:27:11 +0900

    feature/#42: service, createOneLike 로직 조건 추가

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { LikesRepository } from "./likes.repository";
 
 @Injectable()
 export class LikesService {
   constructor(private readonly likesRepository: LikesRepository) {}
 
-  createOneLike(senderNo: number, receiverNo: number) {}
+  async createOneLike(senderNo: number, receiverNo: number) {
+    if (await this.likesRepository.findOneLike(senderNo, receiverNo)) {
+      throw new NotFoundException("sonanda..");
+    }
+    return this.likesRepository.createOneLike(senderNo, receiverNo);
+  }
 
   deleteOneLike(senderNo: number, receiverNo: number) {}
 }
