ad789bc99772efb5b521c94dcd51778209c6b05e
JYE <jo1814@naver.com>
2024-05-30 13:30:56 +0900

    feature/#42: serivce, 예외처리 추가

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { LikesRepository } from "./likes.repository";
 
 @Injectable()
@@ -6,11 +10,17 @@ export class LikesService {
   constructor(private readonly likesRepository: LikesRepository) {}
 
   async createOneLike(senderNo: number, receiverNo: number) {
+    if (senderNo === receiverNo)
+      throw new ForbiddenException("Users can't like themselves alone.");
+
     if (await this.likesRepository.findOneLike(senderNo, receiverNo)) {
-      throw new NotFoundException("sonanda..");
+      throw new NotFoundException("This like doesn't exist.");
     }
+
     return this.likesRepository.createOneLike(senderNo, receiverNo);
   }
 
-  deleteOneLike(senderNo: number, receiverNo: number) {}
+  deleteOneLike(senderNo: number, receiverNo: number) {
+    return this.likesRepository.deleteOneLike(senderNo, receiverNo);
+  }
 }
