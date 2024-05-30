11d7b0708e409b74081572a938118385cfb9b35b
JYE <jo1814@naver.com>
2024-05-30 13:39:57 +0900

    feature/#42: service, receiver 존재하는지 조건 추가

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -14,12 +14,14 @@ export class LikesService {
   ) {}
 
   async createOneLike(senderNo: number, receiverNo: number) {
+    if (!(await this.usersRepository.findUserNicknameByUserNo(receiverNo)))
+      throw new NotFoundException("User doens't exist.");
+
     if (senderNo === receiverNo)
       throw new ForbiddenException("Users can't like themselves alone.");
 
-    if (await this.likesRepository.findOneLike(senderNo, receiverNo)) {
-      throw new NotFoundException("This like doesn't exist.");
-    }
+    if (await this.likesRepository.findOneLike(senderNo, receiverNo))
+      throw new NotFoundException("This like already exist.");
 
     return this.likesRepository.createOneLike(senderNo, receiverNo);
   }
