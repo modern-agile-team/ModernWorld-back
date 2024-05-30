d6c816a94525ae816154ca70a0826a3eb4d42b9c
JYE <jo1814@naver.com>
2024-05-30 20:02:57 +0900

    feature/#44: cont, getAchievements 메서드 기초 구현

--- src/achievements/achievements.controller.ts
+++ src/achievements/achievements.controller.ts
@@ -1,4 +1,7 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 
 @Controller("achievements")
-export class AchievementsController {}
+export class AchievementsController {
+  @Get()
+  getAchievements() {}
+}
