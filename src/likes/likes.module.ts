4a23f588107cc11039f84db07497cf2098a32eeb
JYE <jo1814@naver.com>
2024-05-30 13:31:41 +0900

    feature/#42: module, User 모듈 임포트

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { LikesController } from "./likes.controller";
 import { LikesService } from "./likes.service";
 import { LikesRepository } from "./likes.repository";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
+  imports: [UsersModule],
   controllers: [LikesController],
   providers: [LikesService, LikesRepository],
 })
