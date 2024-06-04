af655c9812f26a8779b668db115faeee7f062c0f
JYE <jo1814@naver.com>
2024-06-04 20:14:17 +0900

    feature/#44: repo getAhcievements 생성

--- src/achievements/achievements.repository.ts
+++ src/achievements/achievements.repository.ts
@@ -4,4 +4,8 @@ import { PrismaService } from "src/prisma/prisma.service";
 @Injectable()
 export class AchievementsRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  getAchievements() {
+    return this.prisma.achievement.findMany({ where: {} });
+  }
 }
