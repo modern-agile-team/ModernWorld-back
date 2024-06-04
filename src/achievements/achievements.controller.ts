05230b7b04be3554188a6b2929c5cc1af9a1f166
JYE <jo1814@naver.com>
2024-06-04 20:24:41 +0900

    feature/#44: swagger tag 추가

--- src/achievements/achievements.controller.ts
+++ src/achievements/achievements.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from "@nestjs/common";
 import { AchievementsService } from "./achievements.service";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller("achievements")
+@ApiTags("Achievements")
 export class AchievementsController {
   constructor(private readonly achievementsService: AchievementsService) {}
 
   @Get()
+  @ApiOperation({
+    summary: "업적 조회",
+  })
   getAchievements() {
     return this.achievementsService.getAchievements();
   }
