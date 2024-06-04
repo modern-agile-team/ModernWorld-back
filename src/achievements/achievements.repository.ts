bc8a3a16cb61a0aad31efeb8a0aa730b1f7db72e
JYE <jo1814@naver.com>
2024-06-04 20:14:45 +0900

    feature/#44: repo getAhcievements 반환타입 생성

--- src/achievements/achievements.repository.ts
+++ src/achievements/achievements.repository.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaPromise, achievement } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class AchievementsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  getAchievements() {
+  getAchievements(): PrismaPromise<achievement[]> {
     return this.prisma.achievement.findMany({ where: {} });
   }
 }
