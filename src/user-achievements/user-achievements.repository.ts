287ba896ab2cc566f9f2351ce260eb16d0f0a574
JYE <jo1814@naver.com>
2024-06-05 12:25:39 +0900

    feature/#44: userAchievements 유저업적 조회, 생성 로직추가

--- src/user-achievements/user-achievements.repository.ts
+++ src/user-achievements/user-achievements.repository.ts
@@ -6,6 +6,28 @@ export class UserAchievementsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   getUserAchievements(userNo: number) {
-    return this.prisma.userAchievement.findMany({ where: { userNo } });
+    return this.prisma.userAchievement.findMany({
+      select: {
+        no: true,
+        userNo: true,
+        achievementNo: true,
+        status: true,
+        achievement: {
+          select: {
+            name: true,
+            description: true,
+            title: true,
+            level: true,
+          },
+        },
+      },
+      where: { userNo },
+    });
+  }
+
+  createOneUserAchievements(userNo: number, achievementNo: number) {
+    return this.prisma.userAchievement.create({
+      data: { userNo, achievementNo },
+    });
   }
 }
