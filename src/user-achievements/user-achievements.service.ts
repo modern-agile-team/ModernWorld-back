f7e691fa5f61c229ec99fad13ce12f88195b6aaa
JYE <jo1814@naver.com>
2024-06-05 14:44:19 +0900

    feature/#44: userAchievements Repo에 achievementRepository 주입

--- src/user-achievements/user-achievements.service.ts
+++ src/user-achievements/user-achievements.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { UserAchievementsRepository } from "./user-achievements.repository";
+import { AchievementsRepository } from "src/achievements/achievements.repository";
 
 @Injectable()
 export class UserAchievementsService {
   constructor(
     private readonly userAchievementsRepository: UserAchievementsRepository,
+    private readonly achievementsRepository: AchievementsRepository,
   ) {}
 
   getUserAchievements(userNo: number) {
