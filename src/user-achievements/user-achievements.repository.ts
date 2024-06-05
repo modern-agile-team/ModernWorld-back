3d57224fe5868f09b8ffadc2cba65d63785acbc1
JYE <jo1814@naver.com>
2024-06-05 11:28:58 +0900

    feature/#44: UserAchievements repo prisma 주입

--- src/user-achievements/user-achievements.repository.ts
+++ src/user-achievements/user-achievements.repository.ts
@@ -1,4 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
-export class UserAchievementsRepository {}
+export class UserAchievementsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+}
