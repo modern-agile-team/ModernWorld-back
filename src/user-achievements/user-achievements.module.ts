54b93e1dcbf619d97081720a7ff24b1695f5c19a
JYE <jo1814@naver.com>
2024-06-05 11:27:32 +0900

    feature/#44: nesg g s

--- src/user-achievements/user-achievements.module.ts
+++ src/user-achievements/user-achievements.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { UserAchievementsController } from "./user-achievements.controller";
 import { UserAchievementsRepository } from "./user-achievements.repository";
+import { UserAchievementsService } from './user-achievements.service';
 
 @Module({
   controllers: [UserAchievementsController],
-  providers: [UserAchievementsRepository],
+  providers: [UserAchievementsRepository, UserAchievementsService],
   exports: [UserAchievementsRepository],
 })
 export class UserAchievementsModule {}
