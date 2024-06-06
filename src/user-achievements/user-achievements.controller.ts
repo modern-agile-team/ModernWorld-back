f1c84a9aa4a80ffb5d80b1217f33a3277084495e
JYE <jo1814@naver.com>
2024-06-06 16:07:23 +0900

    feature/#44: userAchievement Cont, 업적 활성화유무 로직 구현

--- src/user-achievements/user-achievements.controller.ts
+++ src/user-achievements/user-achievements.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Patch } from "@nestjs/common";
 import { UserAchievementsService } from "./user-achievements.service";
 import { ApiTags } from "@nestjs/swagger";
+import { updateUserAchievementStatusDto } from "./dtos/update-user-achievement-status.dto";
 
 @Controller("user-achievements")
 @ApiTags("UserAchievements")
@@ -9,10 +10,20 @@ export class UserAchievementsController {
     private readonly userAchievementsService: UserAchievementsService,
   ) {}
 
-  @Get("")
+  @Get()
   getUserAchievements() {
     const userNo = 1;
 
     return this.userAchievementsService.getUserAchievements(userNo);
+  }
+
+  @Patch()
+  updateUserAchievementStatus(@Body() body: updateUserAchievementStatusDto) {
+    const userNo = 1;
+
+    return this.userAchievementsService.updateUserAchievementStatus(
+      userNo,
+      body,
+    );
   }
 }
