abd2ab21293bd2ffb7c1bc98a5f744188882fd6f
JYE <jo1814@naver.com>
2024-06-06 15:44:52 +0900

    feature/#44: userAchievement service, 업적 레포 주입받던거 제거 및 업적 활성/비활성 로직 추가

--- src/user-achievements/user-achievements.service.ts
+++ src/user-achievements/user-achievements.service.ts
@@ -1,15 +1,48 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { UserAchievementsRepository } from "./user-achievements.repository";
-import { AchievementsRepository } from "src/achievements/achievements.repository";
+import { updateUserAchievementStatusDto } from "./dtos/update-user-achievement-status.dto";
 
 @Injectable()
 export class UserAchievementsService {
   constructor(
     private readonly userAchievementsRepository: UserAchievementsRepository,
-    private readonly achievementsRepository: AchievementsRepository,
   ) {}
 
   getUserAchievements(userNo: number) {
     return this.userAchievementsRepository.getUserAchievements(userNo);
+  }
+
+  async updateUserAchievementStatus(
+    userNo: number,
+    body: updateUserAchievementStatusDto,
+  ) {
+    const { status, achievementNo } = body;
+
+    if (
+      !(await this.userAchievementsRepository.findOneUserAchievement(
+        userNo,
+        achievementNo,
+      ))
+    )
+      throw new NotFoundException("User doesn't have that Achievement.");
+
+    if (!status)
+      return this.userAchievementsRepository.updateUserAchievementStatus(
+        userNo,
+        status,
+        achievementNo,
+      );
+
+    //트랜잭션으로 묶을것
+    await this.userAchievementsRepository.updateUserAchievementStatus(
+      userNo,
+      false,
+    );
+
+    return this.userAchievementsRepository.updateUserAchievementStatus(
+      userNo,
+      status,
+      achievementNo,
+    );
   }
 }
