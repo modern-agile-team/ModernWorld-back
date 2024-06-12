f3ccd965f830829b80037cb0ebe70b78a84f8bb7
cyoure <wwj9559@gmail.com>
2024-06-12 20:59:23 +0900

    refactor/#22: Swagger 폴더 새로 만들기 응답 예시 추가

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -13,18 +13,18 @@ import { CommentService } from "./comment.service";
 import { CreateCommentDto } from "./dto/create-comment.dto";
 import { UpdateCommentDto } from "./dto/update-comment.dto";
 import { GetCommentDto } from "./dto/get-comment.dto";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiTags } from "@nestjs/swagger";
+import { ApiCreateComment } from "./swagger-decorators/create-comment-decorator";
+import { ApiFindComments } from "./swagger-decorators/find-comments-decorator";
+import { ApiUpdateComment } from "./swagger-decorators/update-comment-decorator";
+import { ApiDeleteComment } from "./swagger-decorators/delete-comments-decorator";
 
 @Controller("comments")
 @ApiTags("Comments")
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
-
+  @ApiCreateComment()
   @Post(":receiverNo")
-  @ApiOperation({
-    summary: "방명록 생성하는 API",
-    description: "방명록을 생성합니다.",
-  })
   createComment(
     @Param("receiverNo", ParseIntPipe) receiverNo: number,
     @Body() content: CreateCommentDto,
@@ -34,20 +34,14 @@ export class CommentController {
   }
 
   @Get()
-  @ApiOperation({
-    summary: "방명록 조회하는 API",
-    description: "방명록을 조회합니다.",
-  })
+  @ApiFindComments()
   getComment(@Query() queryParams: GetCommentDto) {
     const userNo = 1;
     return this.commentService.getComment(userNo, queryParams);
   }
 
   @Patch(":commnetNo")
-  @ApiOperation({
-    summary: "방명록 수정하는 API",
-    description: "방명록을 수정합니다.",
-  })
+  @ApiUpdateComment()
   updateComment(
     @Param("commnetNo", ParseIntPipe) commentNo: number,
     @Body() content: UpdateCommentDto,
@@ -56,10 +50,7 @@ export class CommentController {
   }
 
   @Delete(":commnetNo")
-  @ApiOperation({
-    summary: "방명록 삭제하는 API",
-    description: "방명록을 삭제합니다. 하지만 db에는 존재합니다.",
-  })
+  @ApiDeleteComment()
   softDeleteComment(@Param("commnetNo", ParseIntPipe) commentNo: number) {
     return this.commentService.softDeleteComment(commentNo);
   }
