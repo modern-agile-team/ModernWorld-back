f6fb91c5198b02685d5a21505f8284bd4319779e
JYE <jo1814@naver.com>
2024-06-17 16:16:49 +0900

    feature/#51: task(스케줄러), deleteSseConnection 로직 추가

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { Cron } from "@nestjs/schedule";
+import { SseService } from "src/sse/sse.service";
 import { UsersRepository } from "src/users/users.repository";
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly sseService: SseService,
+  ) {}
 
   @Cron("0 0 0 * * 1", {
     timeZone: "Asia/seoul",
@@ -15,7 +19,18 @@ export class TasksService {
     try {
       return this.usersRepository.resetUserAttendance();
     } catch {
-      throw new InternalServerErrorException("transaction error");
+      throw new InternalServerErrorException("Transaction error.");
+    }
+  }
+
+  // 0 * * * * *
+  // 원래는 정각마다 초기화 해주는게 좋을듯, 아래는 10초마다 초기화해줌 10, 20, 30, 40
+  @Cron("*/10 * * * * *")
+  deleteSseConnection() {
+    try {
+      return this.sseService.deleteAllSse();
+    } catch {
+      throw new InternalServerErrorException("Sse initialization error.");
     }
   }
 }
