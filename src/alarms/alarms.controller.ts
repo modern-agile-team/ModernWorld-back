5641a6a433d0d9bcd8ccbabb81778fe19c95c564
JYE <jo1814@naver.com>
2024-06-17 16:45:49 +0900

    feature/#55: Cont, Service 주입

--- src/alarms/alarms.controller.ts
+++ src/alarms/alarms.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+import { AlarmsService } from "./alarms.service";
 
 @Controller("alarms")
-export class AlarmsController {}
+export class AlarmsController {
+  constructor(private readonly alarmsService: AlarmsService) {}
+  @Get()
+  getAllAlarms() {
+    this.alarmsService.getAllAlarms();
+  }
+
+  @Get()
+  getOneAlarm() {}
+}
