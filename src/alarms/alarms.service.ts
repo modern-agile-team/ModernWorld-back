8fa9f38cddbaf5a781a3dc7627f644d8b569ddd5
JYE <jo1814@naver.com>
2024-06-18 12:54:01 +0900

    feature/#55: service, getAllAlarms 로직 추가

--- src/alarms/alarms.service.ts
+++ src/alarms/alarms.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { AlarmsRepository } from "./alarms.repository";
+import { getAllAlarmsDto } from "./dtos/get-all-alarms.dto";
 
 @Injectable()
 export class AlarmsService {
   constructor(private readonly alarmsRepository: AlarmsRepository) {}
-  getAllAlarms() {}
 
-  getOneAlar() {}
+  getAllAlarms(userNo: number, queryParams: getAllAlarmsDto) {
+    const { take, page } = queryParams;
+    const skip = take * (page - 1);
+
+    return this.alarmsRepository.getAllAlarmsByUserNo(userNo, take, skip);
+  }
 }
