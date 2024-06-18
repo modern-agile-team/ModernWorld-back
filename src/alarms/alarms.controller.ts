db4448fb2df88cd362c07c3e7daf2c7b8ff2e87a
JYE <jo1814@naver.com>
2024-06-18 12:52:20 +0900

    feature/#55: Cont, 알람 조회 로직 추가

--- src/alarms/alarms.controller.ts
+++ src/alarms/alarms.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { AlarmsService } from "./alarms.service";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { getAllAlarmsDto } from "./dtos/get-all-alarms.dto";
 
 @Controller("alarms")
+@ApiTags("Alarms")
 export class AlarmsController {
   constructor(private readonly alarmsService: AlarmsService) {}
-  @Get()
-  getAllAlarms() {
-    this.alarmsService.getAllAlarms();
-  }
 
   @Get()
-  getOneAlarm() {}
+  @ApiOperation({ summary: "알람 조회" })
+  getAllAlarms(@Query() queryParams: getAllAlarmsDto) {
+    const userNo = 1;
+
+    return this.alarmsService.getAllAlarms(userNo, queryParams);
+  }
 }
