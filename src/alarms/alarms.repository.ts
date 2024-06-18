a51277e473cb8a9a6ffcf2758c0ee8b3d8e80615
JYE <jo1814@naver.com>
2024-06-18 12:55:28 +0900

    feature/#55: Repo, getAllAlarmsByUserNo, createOneAlarm 로직 추가

--- src/alarms/alarms.repository.ts
+++ src/alarms/alarms.repository.ts
@@ -1,9 +1,32 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaPromise, alarm } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class AlarmsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  getAllAlarmsByUserNo() {}
+  getAllAlarmsByUserNo(
+    userNo: number,
+    take: number,
+    skip: number,
+  ): PrismaPromise<alarm[]> {
+    return this.prisma.alarm.findMany({
+      select: {
+        no: true,
+        userNo: true,
+        content: true,
+        status: true,
+        createdAt: true,
+      },
+      where: { userNo },
+      take,
+      skip,
+      orderBy: { no: "desc" },
+    });
+  }
+
+  createOneAlarm(userNo: number, content: string): PrismaPromise<alarm> {
+    return this.prisma.alarm.create({ data: { userNo, content } });
+  }
 }
