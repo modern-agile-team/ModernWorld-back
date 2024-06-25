339fabd2c3dcebcdb377cd204791aaca9c39b9c7
JYE <jo1814@naver.com>
2024-06-25 13:22:41 +0900

    feature/#51: logger를 이용한 SSE 연결 로깅

--- src/sse/sse.controller.ts
+++ src/sse/sse.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, MessageEvent, Param, Sse } from "@nestjs/common";
+import { Controller, Logger, MessageEvent, Param, Sse } from "@nestjs/common";
 import { Observable, map } from "rxjs";
 import { SseService } from "./sse.service";
 
 @Controller("sse")
 export class SseController {
-  constructor(private readonly sseService: SseService) {}
+  constructor(
+    private readonly logger: Logger,
+    private readonly sseService: SseService,
+  ) {}
 
   //SSE 연결 Route
   @Sse(":userNo")
   sse(@Param("userNo") userNo: string): Observable<MessageEvent> {
-    console.log(`${userNo} : SSE 연결이왔어용`);
+    this.logger.log(`${userNo} : Try SSE connection.`);
     const notifications$ = this.sseService.getSubject(userNo);
 
     return notifications$.pipe(
