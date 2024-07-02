6b70a68dcadbe0b81ba101947077884f71e0adae
JYE <jo1814@naver.com>
2024-07-02 15:18:54 +0900

    feature/#55: Common에 Pagination Dto 생성

---
+++ src/common/dtos/pagination.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsEnum, IsInt, IsPositive } from "class-validator";
+import { OrderBy } from "../enum/order-by.enum";
+
+export class PaginationDto {
+  @ApiProperty({ required: false, default: 1, description: "페이지 번호" })
+  @IsInt()
+  @IsPositive()
+  @Type(() => Number)
+  page: number = 1;
+
+  @ApiProperty({ required: false, default: 10, description: "가져올 자료 수" })
+  @IsInt()
+  @IsPositive()
+  @Type(() => Number)
+  take: number = 10;
+
+  @ApiProperty({
+    required: false,
+    enum: OrderBy,
+    default: OrderBy.DESC,
+    description: "ASC / DESC",
+  })
+  @IsEnum(OrderBy)
+  orderBy: OrderBy = OrderBy.DESC;
+}
