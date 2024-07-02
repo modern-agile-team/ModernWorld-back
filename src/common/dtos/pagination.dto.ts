504dbec59ed66d56dd8817ea66f4a98dd37c35d3
JYE <jo1814@naver.com>
2024-07-02 19:07:07 +0900

    refactor/#55: ApiProperty -> ApiPropertyOptional 변경

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,27 +1,26 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsEnum, IsInt, IsPositive } from "class-validator";
 import { OrderBy } from "../enum/order-by.enum";
 
 export class PaginationDto {
-  @ApiProperty({ required: false, default: 1, description: "페이지 번호" })
+  @ApiPropertyOptional({ default: 1, description: "페이지 번호" })
   @IsInt()
   @IsPositive()
   @Type(() => Number)
-  page: number = 1;
+  page?: number = 1;
 
-  @ApiProperty({ required: false, default: 10, description: "가져올 자료 수" })
+  @ApiPropertyOptional({ default: 10, description: "가져올 자료 수" })
   @IsInt()
   @IsPositive()
   @Type(() => Number)
-  take: number = 10;
+  take?: number = 10;
 
-  @ApiProperty({
-    required: false,
+  @ApiPropertyOptional({
     enum: OrderBy,
     default: OrderBy.DESC,
     description: "asc / desc",
   })
   @IsEnum(OrderBy)
-  orderBy: OrderBy = OrderBy.DESC;
+  orderBy?: OrderBy = OrderBy.DESC;
 }
