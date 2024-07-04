72b0c1c600c6acc435138993dc4e7e677332863b
JYE <jo1814@naver.com>
2024-07-04 15:53:14 +0900

    feature/#44: update legend count 관련 type, interface 추가

--- src/legends/types/update-legend.type.ts
+++ src/legends/types/update-legend.type.ts
@@ -1,18 +1,5 @@
-type LegendField = {
-  itemCount: string;
-  attendanceCount: string;
-  likeCount: string;
-  commentCount: string;
-  presentCount: string;
-};
+import { UpdateLegendCount } from "../interfaces/update-legend-count.interface";
 
-type UpdateCount<T extends keyof LegendField> = {
-  [K in T]: { increment: number };
+export type SingleLegendField = {
+  [K in keyof UpdateLegendCount]?: UpdateLegendCount[K];
 };
-
-export type UpdateLegendType =
-  | UpdateCount<"itemCount">
-  | UpdateCount<"attendanceCount">
-  | UpdateCount<"likeCount">
-  | UpdateCount<"commentCount">
-  | UpdateCount<"presentCount">;
