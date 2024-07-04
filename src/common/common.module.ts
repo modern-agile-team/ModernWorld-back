13a79d0bf136089ab060f07e015c8f13617ae20e
JYE <jo1814@naver.com>
2024-07-04 15:50:51 +0900

    feature/#44: Common Module, 여러 모듈 추가

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,21 @@
-import { Module } from '@nestjs/common';
-import { CommonService } from './common.service';
+import { Module } from "@nestjs/common";
+import { CommonService } from "./common.service";
+import { UsersModule } from "src/users/users.module";
+import { LegendsModule } from "src/legends/legends.module";
+import { UserAchievementsModule } from "src/user-achievements/user-achievements.module";
+import { SseModule } from "src/sse/sse.module";
+import { AlarmsModule } from "src/alarms/alarms.module";
+import { CommonController } from "./common.controller";
 
 @Module({
-  providers: [CommonService]
+  imports: [
+    UsersModule,
+    LegendsModule,
+    UserAchievementsModule,
+    SseModule,
+    AlarmsModule,
+  ],
+  providers: [CommonService],
+  exports: [CommonService],
 })
 export class CommonModule {}
