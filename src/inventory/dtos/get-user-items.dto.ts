80df09763e9758839f493d4b3e7343f19be4eb40
JYE <jo1814@naver.com>
2024-07-09 10:42:15 +0900

    feature/#62: inventory Dto 추가 및 swagger ApiProperty 수정

--- src/inventory/dtos/get-user-items.dto.ts
+++ src/inventory/dtos/get-user-items.dto.ts
@@ -4,16 +4,10 @@ import { Transform } from "class-transformer";
 import { IsBoolean, IsOptional } from "class-validator";
 
 export class GetUserItemsDto {
-  @ApiPropertyOptional({
-    name: "theme",
-    example: "테마이름",
-  })
+  @ApiPropertyOptional({ description: "테마이름" })
   theme?: string;
 
-  @ApiPropertyOptional({
-    name: "status",
-    example: true,
-  })
+  @ApiPropertyOptional({ description: "사용 여부" })
   @IsBoolean()
   @Transform(({ value }) => {
     return value === "true"
@@ -25,9 +19,6 @@ export class GetUserItemsDto {
   @IsOptional()
   status?: boolean;
 
-  @ApiPropertyOptional({
-    name: "itemName",
-    example: "왕덕봉",
-  })
+  @ApiPropertyOptional()
   itemName?: string;
 }
