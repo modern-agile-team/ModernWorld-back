e55c438a5539ad63ea7a08681fd95717cfd32017
JYE <jo1814@naver.com>
2024-07-09 19:18:43 +0900

    delete/#62: updateUserDescriptionDto APi property 몇개 옵션 삭제

--- src/users/dtos/update-user-description.dto.ts
+++ src/users/dtos/update-user-description.dto.ts
@@ -3,9 +3,7 @@ import { MaxLength } from "class-validator";
 
 export class UpdateUserDescriptionDto {
   @ApiProperty({
-    name: "description",
     description: "자기소개",
-    example: "어떻게 사람이름이 엄준식",
     maximum: 100,
   })
   @MaxLength(100)
