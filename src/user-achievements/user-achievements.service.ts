bdd241ea67dbd5130a37f16f003e1174d6b8420b
JYE <jo1814@naver.com>
2024-07-12 10:41:03 +0900

    feature/#62: userAchievement service, 업적상태 변경 로직 변경 및 트랜잭션 추가

--- src/user-achievements/user-achievements.service.ts
+++ src/user-achievements/user-achievements.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { UserAchievementsRepository } from "./user-achievements.repository";
 import { updateUserAchievementStatusDto } from "./dtos/update-user-achievement-status.dto";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class UserAchievementsService {
   constructor(
+    private readonly prisma: PrismaService,
     private readonly userAchievementsRepository: UserAchievementsRepository,
   ) {}
 
@@ -14,35 +16,38 @@ export class UserAchievementsService {
 
   async updateUserAchievementStatus(
     userNo: number,
+    achievementNo: number,
     body: updateUserAchievementStatusDto,
   ) {
-    const { status, achievementNo } = body;
-
-    if (
-      !(await this.userAchievementsRepository.findOneUserAchievement(
+    const { status } = body;
+    const userAchievement =
+      await this.userAchievementsRepository.findOneUserAchievement(
         userNo,
-        achievementNo,
-      ))
-    )
-      throw new NotFoundException("User doesn't have that Achievement.");
-
-    if (!status)
-      return this.userAchievementsRepository.updateUserAchievementStatus(
-        userNo,
-        status,
         achievementNo,
       );
 
-    //트랜잭션으로 묶을것
-    await this.userAchievementsRepository.updateUserAchievementStatus(
-      userNo,
-      false,
-    );
+    if (!userAchievement)
+      throw new NotFoundException("User doesn't have that Achievement.");
 
-    return this.userAchievementsRepository.updateUserAchievementStatus(
-      userNo,
-      status,
-      achievementNo,
-    );
+    if (!status)
+      return this.userAchievementsRepository.updateUserAchievementStatusByNo(
+        userAchievement.no,
+        status,
+      );
+
+    //트랜잭션으로 묶을것
+    const [, result] = await this.prisma.$transaction([
+      this.userAchievementsRepository.updateUserAchievementStatus(
+        userNo,
+        false,
+      ),
+
+      this.userAchievementsRepository.updateUserAchievementStatusByNo(
+        userAchievement.no,
+        status,
+      ),
+    ]);
+
+    return result;
   }
 }
