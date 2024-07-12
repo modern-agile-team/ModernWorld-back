7c4d23f7471dc6e2a4c864fefe65470ebd075931
JYE <jo1814@naver.com>
2024-07-12 13:45:48 +0900

    refactor/#62: get user items DTO BooeleanTransform 적용

--- src/inventory/dtos/get-user-items.dto.ts
+++ src/inventory/dtos/get-user-items.dto.ts
@@ -1,21 +1,13 @@
-import { BadRequestException } from "@nestjs/common";
 import { ApiPropertyOptional } from "@nestjs/swagger";
-import { Transform } from "class-transformer";
-import { IsBoolean, IsOptional } from "class-validator";
+import { IsOptional } from "class-validator";
+import { BooleanTransform } from "src/common/decorators/boolean-transform.decorator";
 
 export class GetUserItemsDto {
   @ApiPropertyOptional({ description: "테마이름" })
   theme?: string;
 
   @ApiPropertyOptional({ description: "사용 여부" })
-  @IsBoolean()
-  @Transform(({ value }) => {
-    return value === "true"
-      ? true
-      : value === "false"
-        ? false
-        : BadRequestException;
-  })
+  @BooleanTransform()
   @IsOptional()
   status?: boolean;
 
