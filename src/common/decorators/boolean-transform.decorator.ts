0e059a81e0999955c767a2f80ebd2f3ab5a18ed9
JYE <jo1814@naver.com>
2024-07-12 13:40:07 +0900

    feature/#62: booleanTransFform decorator 생성

---
+++ src/common/decorators/boolean-transform.decorator.ts
@@ -0,0 +1,14 @@
+import { Transform } from "class-transformer";
+import { BadRequestException } from "@nestjs/common";
+
+export function BooleanTransform() {
+  return Transform(({ value }) => {
+    if (value === "true") {
+      return true;
+    } else if (value === "false") {
+      return false;
+    } else {
+      throw new BadRequestException("Invalid boolean value");
+    }
+  });
+}
