37b2fee67ed80d4479f04ffb4544c9dd166af148
JYE <jo1814@naver.com>
2024-07-12 10:41:46 +0900

    modify/#62: userAchievement Cont, achievementNo body로 받던것 param으로 받게끔 수정

--- src/user-achievements/user-achievements.controller.ts
+++ src/user-achievements/user-achievements.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Controller, Get, Patch } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+} from "@nestjs/common";
 import { UserAchievementsService } from "./user-achievements.service";
 import { ApiTags } from "@nestjs/swagger";
 import { updateUserAchievementStatusDto } from "./dtos/update-user-achievement-status.dto";
+import { ApiUpdateUserAchievementStatus } from "./userAchievements-swagger/update-user-achievement-status.decorator";
 
-@Controller("user-achievements")
+@Controller("users/achievements")
 @ApiTags("UserAchievements")
 export class UserAchievementsController {
   constructor(
@@ -17,12 +25,18 @@ export class UserAchievementsController {
     return this.userAchievementsService.getUserAchievements(userNo);
   }
 
-  @Patch()
-  updateUserAchievementStatus(@Body() body: updateUserAchievementStatusDto) {
+  @Patch(":achievementNo")
+  @ApiUpdateUserAchievementStatus()
+  updateUserAchievementStatus(
+    @Param("achievementNo", ParseIntPipe) achievementNo: number,
+    @Body()
+    body: updateUserAchievementStatusDto,
+  ) {
     const userNo = 1;
 
     return this.userAchievementsService.updateUserAchievementStatus(
       userNo,
+      achievementNo,
       body,
     );
   }
