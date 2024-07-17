e1e5419caaeb3870cde59ac60e1ef50bcccc893e
JYE <jo1814@naver.com>
2024-07-17 17:52:30 +0900

    feature/#44: create comment deco, 댓글 생성 example 추가

--- src/comments/swagger-decorators/comment-swagger/create-comment.decorator.ts
+++ src/comments/swagger-decorators/comment-swagger/create-comment.decorator.ts
@@ -1,8 +1,9 @@
 import { applyDecorators } from "@nestjs/common";
 import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
   ApiInternalServerErrorResponse,
   ApiOperation,
-  ApiResponse,
 } from "@nestjs/swagger";
 
 export function ApiCreateComment() {
@@ -11,8 +12,8 @@ export function ApiCreateComment() {
       summary: "방명록 생성하는 API",
       description: "방명록을 생성합니다.",
     }),
-    ApiResponse({
-      status: 201,
+
+    ApiCreatedResponse({
       description: "댓글을 성공적으로 생성한 경우",
       content: {
         JSON: {
@@ -23,6 +24,34 @@ export function ApiCreateComment() {
             content: "모던애자일 사랑해요.",
             createdAt: "2024-06-26T08:36:44.000Z",
             deletedAt: null,
+          },
+        },
+      },
+    }),
+
+    ApiBadRequestResponse({
+      content: {
+        JSON: {
+          examples: {
+            ex1: {
+              summary: "body의 content가 1자 이상 100자 이하가 아닌 경우",
+              value: {
+                message: [
+                  "content must be longer than or equal to 1 and shorter than or equal to 100 characters",
+                ],
+                error: "Bad Request",
+                statusCode: 400,
+              },
+            },
+
+            ex2: {
+              summary: "param의 userNo가 양의 정수가 아닌 경우",
+              value: {
+                message: "Validation failed (positive int string is expected)",
+                error: "Bad Request",
+                statusCode: 400,
+              },
+            },
           },
         },
       },
