3d98ade13efecd34f5c2ebc2a5b25eacfac5b054
jinwoo0207 <a91287840@gmail.com>
2024-07-18 15:45:37 +0900

    feature/#69: 쿠키 인터셉터 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
   Req,
   Res,
   UseGuards,
+  UseInterceptors,
 } from "@nestjs/common";
 import { AuthService } from "./services/auth.service";
 import { AccessTokenAuthGuard, RefreshTokenAuthGuard } from "./jwt/jwt.guard";
@@ -16,6 +17,7 @@ import { TokenService } from "./services/token.service";
 import { ApiNaverLogin } from "./swagger-decorators/naver-login-decorator";
 import { ApiKakaoLogin } from "./swagger-decorators/kakao-login-decorator";
 import { ApiNewAccessToken } from "./swagger-decorators/new-access-token.decorator";
+import { CookieInterceptor } from "./cookie.interceptor";
 
 @Controller("auth")
 export class AuthController {
@@ -25,37 +27,23 @@ export class AuthController {
   ) {}
 
   @ApiNaverLogin()
+  @UseInterceptors(CookieInterceptor)
   @Post("naver/login")
-  async naverLogin(@Query("code") code: string, @Res() res: Response) {
+  async naverLogin(@Query("code") code: string) {
     if (!code) {
       throw new BadRequestException("인가 코드가 없습니다.");
     }
-    const token = await this.authService.naverLogin(code);
-    res.cookie("refreshToken", token.refreshToken, {
-      domain: "localhost",
-      path: "/",
-      httpOnly: true,
-      secure: false, // https 설정을 확인하지 않기 위해서 선언
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    });
-    return res.send(token);
+    return this.authService.naverLogin(code);
   }
 
   @ApiKakaoLogin()
+  @UseInterceptors(CookieInterceptor)
   @Post("kakao/login")
-  async kakaoLogin(@Query("code") code: string, @Res() res: Response) {
+  async kakaoLogin(@Query("code") code: string) {
     if (!code) {
       throw new BadRequestException("인가 코드가 없습니다.");
     }
-    const token = await this.authService.kakaoLogin(code);
-    res.cookie("refreshToken", token.refreshToken, {
-      domain: "localhost", // 추후 프론트 서버 주소로 변경
-      path: "/",
-      httpOnly: true,
-      secure: false, // https 설정을 확인하지 않기 위해서 선언
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    });
-    return res.send(token);
+    return this.authService.kakaoLogin(code);
   }
   @ApiNewAccessToken()
   @UseGuards(RefreshTokenAuthGuard)
