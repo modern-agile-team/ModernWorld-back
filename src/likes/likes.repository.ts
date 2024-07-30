dc90faf628dfb54c44e7fd5f4b6463ab791ccf71
JYE <jo1814@naver.com>
2024-07-30 11:04:15 +0900

    feature/#98: createOneLike 관련 tx객체 인자로 추가

--- src/likes/likes.repository.ts
+++ src/likes/likes.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaTxType } from "src/prisma/prisma.type";
 
 @Injectable()
 export class LikesRepository {
@@ -9,8 +10,8 @@ export class LikesRepository {
     return this.prisma.like.findFirst({ where: { senderNo, receiverNo } });
   }
 
-  createOneLike(senderNo: number, receiverNo: number) {
-    return this.prisma.like.create({
+  createOneLike(senderNo: number, receiverNo: number, tx?: PrismaTxType) {
+    return (tx ?? this.prisma).like.create({
       select: {
         no: true,
         userLikeSenderNo: { select: { no: true, nickname: true } },
