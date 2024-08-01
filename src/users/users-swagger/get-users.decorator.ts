622028708707d14970fe5fd941f4d9fe6c0cf8dc
JYE <jo1814@naver.com>
2024-08-01 16:18:13 +0900

    feature/#87: 유저 스웨거 예시 추가

--- src/users/users-swagger/get-users.decorator.ts
+++ src/users/users-swagger/get-users.decorator.ts
@@ -19,6 +19,131 @@ export function ApiGetUsers() {
           example: {
             data: [
               {
+                no: 34,
+                nickname: "asd",
+                description: null,
+                createdAt: "2024-07-26T11:48:35.000Z",
+                accumulationPoint: 0,
+                legend: null,
+                userAchievement: [],
+                characterLocker: [],
+              },
+              {
+                no: 1,
+                nickname: "1",
+                description: null,
+                createdAt: "2024-07-26T11:48:07.000Z",
+                accumulationPoint: 0,
+                legend: {
+                  likeCount: 1,
+                },
+                userAchievement: [],
+                characterLocker: [],
+              },
+              {
+                no: 31,
+                nickname: "쌉악질",
+                description: "김은우 학살자",
+                createdAt: "2024-07-25T07:57:17.000Z",
+                accumulationPoint: 10300,
+                legend: {
+                  likeCount: 0,
+                },
+                userAchievement: [],
+                characterLocker: [],
+              },
+              {
+                no: 30,
+                nickname: "30번닉네임",
+                description: "30번소개",
+                createdAt: "2024-07-01T05:00:46.000Z",
+                accumulationPoint: 0,
+                legend: {
+                  likeCount: 2,
+                },
+                userAchievement: [],
+                characterLocker: [
+                  {
+                    character: {
+                      image: "3번 cat 이미지",
+                    },
+                  },
+                ],
+              },
+              {
+                no: 29,
+                nickname: "29번닉네임",
+                description: "29번소개",
+                createdAt: "2024-07-01T05:00:45.000Z",
+                accumulationPoint: 0,
+                legend: {
+                  likeCount: 2,
+                },
+                userAchievement: [],
+                characterLocker: [
+                  {
+                    character: {
+                      image: "2번 dog 이미지",
+                    },
+                  },
+                ],
+              },
+              {
+                no: 28,
+                nickname: "28번닉네임",
+                description: "28번소개",
+                createdAt: "2024-07-01T05:00:45.000Z",
+                accumulationPoint: 0,
+                legend: {
+                  likeCount: 2,
+                },
+                userAchievement: [],
+                characterLocker: [
+                  {
+                    character: {
+                      image: "1번 cat 이미지",
+                    },
+                  },
+                ],
+              },
+              {
+                no: 27,
+                nickname: "27번닉네임",
+                description: "27번소개",
+                createdAt: "2024-07-01T05:00:45.000Z",
+                accumulationPoint: 0,
+                legend: {
+                  likeCount: 2,
+                },
+                userAchievement: [],
+                characterLocker: [
+                  {
+                    character: {
+                      image: "3번 cat 이미지",
+                    },
+                  },
+                ],
+              },
+              {
+                no: 26,
+                nickname: "26번닉네임",
+                description: "26번소개",
+                createdAt: "2024-07-01T05:00:45.000Z",
+                accumulationPoint: 0,
+                legend: {
+                  likeCount: 2,
+                },
+                userAchievement: [],
+                characterLocker: [
+                  {
+                    character: {
+                      image: "2번 dog 이미지",
+                    },
+                  },
+                ],
+              },
+              {
+                no: 25,
                 nickname: "25번닉네임",
                 description: "25번소개",
                 createdAt: "2024-07-01T05:00:45.000Z",
@@ -36,6 +161,7 @@ export function ApiGetUsers() {
                 ],
               },
               {
+                no: 24,
                 nickname: "24번닉네임",
                 description: "24번소개",
                 createdAt: "2024-07-01T05:00:45.000Z",
@@ -52,63 +178,12 @@ export function ApiGetUsers() {
                   },
                 ],
               },
-              {
-                nickname: "23번닉네임",
-                description: "23번소개",
-                createdAt: "2024-07-01T05:00:45.000Z",
-                accumulationPoint: 0,
-                legend: {
-                  likeCount: 2,
-                },
-                userAchievement: [],
-                characterLocker: [
-                  {
-                    character: {
-                      image: "7번 dog 이미지",
-                    },
-                  },
-                ],
-              },
-              {
-                nickname: "22번닉네임",
-                description: "22번소개",
-                createdAt: "2024-07-01T05:00:45.000Z",
-                accumulationPoint: 0,
-                legend: {
-                  likeCount: 2,
-                },
-                userAchievement: [],
-                characterLocker: [
-                  {
-                    character: {
-                      image: "5번 dog 이미지",
-                    },
-                  },
-                ],
-              },
-              {
-                nickname: "21번닉네임",
-                description: "21번소개",
-                createdAt: "2024-07-01T05:00:45.000Z",
-                accumulationPoint: 0,
-                legend: {
-                  likeCount: 2,
-                },
-                userAchievement: [],
-                characterLocker: [
-                  {
-                    character: {
-                      image: "2번 dog 이미지",
-                    },
-                  },
-                ],
-              },
             ],
             meta: {
-              page: 2,
-              take: 5,
-              totalCount: 30,
-              totalPage: 6,
+              page: 1,
+              take: 10,
+              totalCount: 32,
+              totalPage: 4,
             },
           },
         },
