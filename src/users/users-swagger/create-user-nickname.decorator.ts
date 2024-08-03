9d066f7e03b20d235d5bbb3daf71a70c19ed1a61
JYE <jo1814@naver.com>
2024-08-03 20:29:54 +0900

    modify/#107: 유저 닉네임 생성 로직 정규표현식을 통한 validation 구현

--- src/users/users-swagger/create-user-nickname.decorator.ts
+++ src/users/users-swagger/create-user-nickname.decorator.ts
@@ -31,38 +31,12 @@ export function ApiCreateUserNickname() {
       description: "BadRequest",
       content: {
         JSON: {
-          examples: {
-            ex1: {
-              summary: "10글자 이상의 닉네임을 줬을때",
-              value: {
-                message: [
-                  "nickname must be shorter than or equal to 10 characters",
-                ],
-                error: "Bad Request",
-                statusCode: 400,
-              },
-            },
-            ex2: {
-              summary: "1글자도 주지 않았을 때",
-              value: {
-                message: [
-                  "nickname must be longer than or equal to 1 characters",
-                ],
-                error: "Bad Request",
-                statusCode: 400,
-              },
-            },
-            ex3: {
-              summary: "string 형태가 아닐 때 혹은 그 밖에",
-              value: {
-                message: [
-                  "nickname must be shorter than or equal to 10 characters",
-                  "nickname must be longer than or equal to 1 characters",
-                ],
-                error: "Bad Request",
-                statusCode: 400,
-              },
-            },
+          example: {
+            message: [
+              "nickname must match /\\/^[a-zA-Z가-힣0-9]{2,10}$\\// regular expression",
+            ],
+            error: "Bad Request",
+            statusCode: 400,
           },
         },
       },
