77bb0daf7757a30ad9ea2fc746d41843258c2fbb
JYE <jo1814@naver.com>
2024-08-08 19:21:13 +0900

    feature/#106: commonService에 있던 로직 userAchievements로 옮김

--- src/user-achievements/user-achievements.module.ts
+++ src/user-achievements/user-achievements.module.ts
@@ -1,13 +1,24 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { UserAchievementsController } from "./user-achievements.controller";
 import { UserAchievementsRepository } from "./user-achievements.repository";
 import { UserAchievementsService } from "./user-achievements.service";
 import { AchievementsModule } from "src/achievements/achievements.module";
+import { LegendsModule } from "src/legends/legends.module";
+import { SseModule } from "src/sse/sse.module";
+import { AlarmsModule } from "src/alarms/alarms.module";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
-  imports: [AchievementsModule],
+  imports: [
+    forwardRef(() => UsersModule),
+    AchievementsModule,
+    SseModule,
+    AlarmsModule,
+    AchievementsModule,
+    LegendsModule,
+  ],
   controllers: [UserAchievementsController],
   providers: [UserAchievementsRepository, UserAchievementsService],
-  exports: [UserAchievementsRepository],
+  exports: [UserAchievementsRepository, UserAchievementsService],
 })
 export class UserAchievementsModule {}
