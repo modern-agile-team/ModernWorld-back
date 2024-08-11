8ca0b072d0d6548d08c127e7e59a8f3c5ace7382
JYE <jo1814@naver.com>
2024-08-11 17:22:25 +0900

    refactor/#106: update-present-status-response.dto 이름 변경

--- src/presents/dtos/presents-without-delete-response.dto.ts
+++ src/presents/dtos/presents-without-delete-response.dto.ts
@@ -1,13 +1,13 @@
 import { Prisma } from "@prisma/client";
 import { PresentsRepository } from "../presents.repository";
-import { presentDto } from "./present.dto";
+import { PresentDto } from "./present.dto";
 import { Exclude } from "class-transformer";
 
 type updateOnePresentStatusType = Prisma.PromiseReturnType<
   typeof PresentsRepository.prototype.updateOnePresentStatus
 >;
 
-export class UpdatePresentsStatusResponseDto extends presentDto<updateOnePresentStatusType> {
+export class PresentsWithoutDeleteResponseDto extends PresentDto<updateOnePresentStatusType> {
   constructor(present: updateOnePresentStatusType) {
     super(present);
   }
