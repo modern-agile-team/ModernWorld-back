8cf39392f0221d88943722aa99d23c0710b2ab8a
JYE <jo1814@naver.com>
2024-08-16 17:49:31 +0900

    feature/#126: ban Cont, userNo 받도록 가드 추가

--- src/bans/bans.controller.ts
+++ src/bans/bans.controller.ts
@@ -1,15 +1,18 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { BansService } from "./bans.service";
 import { CreateBanDto } from "./dtos/create-ban.dto";
 import { ApiCreateBan } from "./bans-swagger/create-ban.decorator";
+import { AccessTokenAuthGuard } from "src/auth/jwt/jwt.guard";
+import { UserNo } from "src/auth/auth.decorator";
 
 @Controller("bans")
 export class BansController {
   constructor(private readonly bansService: BansService) {}
 
   @Post()
+  @UseGuards(AccessTokenAuthGuard)
   @ApiCreateBan()
-  createBan(@Body() body: CreateBanDto) {
-    return this.bansService.createBan(body);
+  createBan(@UserNo() adminNo: number, @Body() body: CreateBanDto) {
+    return this.bansService.createBan(adminNo, body);
   }
 }
