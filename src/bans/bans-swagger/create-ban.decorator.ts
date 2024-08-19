12884824fc1886ef7cb653cbd3d4b0c8362270d6
JYE <jo1814@naver.com>
2024-08-19 17:19:41 +0900

    feature/#126: ApiCreateBan example 추가

--- src/bans/bans-swagger/create-ban.decorator.ts
+++ src/bans/bans-swagger/create-ban.decorator.ts
@@ -10,14 +10,22 @@ import {
 export function ApiCreateBan() {
   return applyDecorators(
     ApiOperation({
-      summary: "캐릭터 불러오기",
-      description: "유저가 보유한 것과 상관없이 캐릭터만 불러옵니다.",
+      summary: "유저 밴하기",
+      description: "너 밴",
     }),
 
     ApiOkResponse({
       description: "Success",
       content: {
-        JSON: {},
+        JSON: {
+          example: {
+            no: 14,
+            uniqueIdentifier: "13번고유식별자",
+            content: "string",
+            createdAt: "2024-08-19T05:31:10.000Z",
+            expiredAt: "2024-08-20T05:31:10.000Z",
+          },
+        },
       },
     }),
 
@@ -25,10 +33,29 @@ export function ApiCreateBan() {
       description: "cat, dog 이외의 값을 넣었을 때",
       content: {
         JSON: {
-          example: {
-            message: ["species must be one of the following values: cat, dog"],
-            error: "Bad Request",
-            statusCode: 400,
+          examples: {
+            ex1: {
+              summary: "userNo가 양의 정수가 아닐 경우",
+              value: {
+                message: [
+                  "userNo must be a positive number",
+                  "userNo must be an integer number",
+                ],
+                error: "Bad Request",
+                statusCode: 400,
+              },
+            },
+
+            ex2: {
+              summary: "content가 1자 이상 100자 이하가 아닌 경우",
+              value: {
+                message: [
+                  "content must be longer than or equal to 1 and shorter than or equal to 100 characters",
+                ],
+                error: "Bad Request",
+                statusCode: 400,
+              },
+            },
           },
         },
       },
