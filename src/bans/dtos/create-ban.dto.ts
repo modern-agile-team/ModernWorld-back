2316fdecbb55788cfce032afac20b49d40af604a
JYE <jo1814@naver.com>
2024-08-19 14:24:04 +0900

    feature/#126: CreateBanDto, 명세서 세부 설정

--- src/bans/dtos/create-ban.dto.ts
+++ src/bans/dtos/create-ban.dto.ts
@@ -2,16 +2,16 @@ import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { IsInt, IsOptional, IsPositive, Length } from "class-validator";
 
 export class CreateBanDto {
-  @ApiProperty()
+  @ApiProperty({ minimum: 1 })
   @IsInt()
   @IsPositive()
   userNo: number;
 
-  @ApiProperty()
+  @ApiProperty({ minimum: 1, maximum: 100 })
   @Length(1, 100)
   content: string;
 
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({ minimum: 1 })
   @IsOptional()
   @IsInt()
   @IsPositive()
