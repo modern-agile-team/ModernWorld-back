4c4d19e49c270881a1d73bbac5c601c5e2026202
jinwoo0207 <a91287840@gmail.com>
2024-08-28 13:51:18 +0900

    로그 수정

--- src/auth/jwt/jwt.guard.ts
+++ src/auth/jwt/jwt.guard.ts
@@ -76,8 +76,8 @@ export class RefreshTokenAuthGuard extends AuthGuard("refreshToken") {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log(1 + request.cookies);
-    console.log(2 + request);
+    this.logger.log(request.cookies);
+    this.logger.log(request);
     const authorization = request.cookies["refreshToken"];
     if (!authorization) {
       throw new BadRequestException("jwt must be provided");
