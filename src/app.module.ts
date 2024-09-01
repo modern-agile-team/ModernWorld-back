7a94158a1532929d051bfe2b85e99b15d1ac323a
JYE <jo1814@naver.com>
2024-09-01 17:25:49 +0900

    delete/#143: 안쓰는 appService, appController 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ItemsModule } from "./items/items.module";
 import { InventoryModule } from "./inventory/inventory.module";
@@ -56,8 +54,7 @@ import { ReportsModule } from "./reports/reports.module";
     ReportsModule,
   ],
   // isGlobal: ConfigModule을 AppModul 이외의 모듈에서 반복 import할 필요 없는 전역 모듈로 설정.
-  controllers: [AppController],
-  providers: [AppService, ConfigService],
+  providers: [ConfigService],
   // 앵간하면 nestJS에서 권장하는 방법인 ConfigService를 이용하여 환경변수 값을 사용해야함.
 })
 export class AppModule {}
